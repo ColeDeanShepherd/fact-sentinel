af8b763206ca446f1a40e0f2506e59aded963760
Cole Shepherd <cole@coledeanshepherd.com>
2025-02-17 12:37:40 -0800

    Get post info.

--- background.js
+++ background.js
@@ -1,5 +1,70 @@
+async function getRedditPosts(activeTab) {
+  const results = await chrome.scripting.executeScript({
+    target: { tabId: activeTab.id },
+    func: getRedditPostsScriptFn
+  });
+
+  if (results.length !== 1) {
+    throw new Error("Unexpected number of results");
+  }
+
+  const result = results[0];
+  if (result.error !== undefined) {
+    throw new Error(result.error);
+  }
+
+  return result.result;
+}
+
+function getRedditPostsScriptFn() {
+  const as = Array.from(document.querySelectorAll('a[slot="title"]'));
+
+  return as.map(a => ({
+    title: a.innerText,
+    href: a.href
+  }));
+}
+
+let isRunning = false;
+
+async function startRunning() {
+  // Ensure not already running.
+  if (isRunning) {
+    console.warn("Already running");
+    return;
+  }
+
+  // Get the active tab.
+  const activeTab = (await chrome.tabs.query({ active: true, currentWindow: true }))[0];
+
+  // Redirect to Reddit if not already there.
+  if (!activeTab.url.includes("reddit.com")) {
+    await chrome.tabs.update(activeTab.id, { url: "https://www.reddit.com/" });
+  }
+
+  // Get all Reddit posts.
+  const posts = await getRedditPosts(activeTab);
+
+  console.log('Posts: ', posts);
+}
+
+async function stopRunning() {
+  // Ensure already running.
+  if (!isRunning) {
+    console.warn("Already stopped");
+    return;
+  }
+}
+
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  console.log('Got message in background.js', message);
+  if (message.action === "start") {
+    startRunning();
+  } else if (message.action === "stop") {
+    stopRunning();
+  } else {
+    console.error(`Unknown action: ${message.action}`);
+  }
+
   sendResponse({ status: "got it" });
 });
 
